f3096a8637e25b2065ad8c3144abd5e6b74e2cf0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-19 17:25:00 -0300

    Project created

---
+++ src/main/java/com/lucasmonteiro/web_services_springboot/WebServicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmonteiro.web_services_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmonteiro/web_services_springboot/WebServicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmonteiro.web_services_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
