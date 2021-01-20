40f078d0cafecfe4ea398a6841b0d0aed0fa2d19
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-19 22:05:22 -0300

    Service layer, component registration

--- src/main/java/com/lucasmonteiro/web_services_springboot/repositories/UserRepository.java
+++ src/main/java/com/lucasmonteiro/web_services_springboot/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.lucasmonteiro.web_services_springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lucasmonteiro.web_services_springboot.entities.User;
 
+@Repository // A anotação @Repository é opcional pois o UserRepository está herdando do JpaRepository que já está registrado como componente do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
