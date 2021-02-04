3954610ce9425001a0b8972170a22ddfd5410c2b
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-04 11:44:49 -0300

    User update

--- src/main/java/com/lucasmonteiro/web_services_springboot/resources/UserResource.java
+++ src/main/java/com/lucasmonteiro/web_services_springboot/resources/UserResource.java
@@ -3,12 +3,15 @@ package com.lucasmonteiro.web_services_springboot.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.lucasmonteiro.web_services_springboot.entities.User;
 import com.lucasmonteiro.web_services_springboot.services.UserService;
 
+@Transactional
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -48,6 +52,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
